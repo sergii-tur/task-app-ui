9c64f3083ba452a214bef78042df9cf95600c382
Sergii Tur <sergii.developer@gmail.com>
2023-09-12 18:29:13 +0300

    Tasks & info section UI, new task button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 window.onload = (event) => {
-  console.log("Page loaded");
+
+console.log("Page loaded");
+
 };
+
+function checkboxToggle(element) {
+  const taskTxtElement = element.parentElement.children[2];
+  taskTxtElement.classList.toggle("task-done");
+}
