5ab0276b81128739cffd2a4f30b3e0ed1d38c4b9
Sergii Tur <sergii.developer@gmail.com>
2023-09-19 21:52:19 +0300

    Draft: task buttons onclick functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-window.onload = (event) => {
-
-console.log("Page loaded");
-
-};
-
 function checkboxToggle(element) {
   const taskTxtElement = element.parentElement.children[2];
   taskTxtElement.classList.toggle("task-done");
+}
+
+function addEventToAllTaskButtons() {
+  const taskButtons = document.getElementsByClassName('task-btn');
+  //console.log(taskButtons);
+  for (let i = 0; i < taskButtons.length; i++) {
+    taskButtons[i].addEventListener('click', function(){
+      const parent = taskButtons[i].parentElement.querySelector(".task-menu-ctn");
+      parent.classList.remove("hide");
+      console.log(parent);
+      //alert(this.value);
+    }, false);
+    //console.log(taskButtons[i]);
+  }
+}
+
+addEventToAllTaskButtons();
+
+function closeTaskMenu(element) {
+  const parent = element.parentElement;
+  parent.classList.add("hide");
+}
+
+function editTask(element) {
+  console.log("EDIT TASK");
+}
+
+function deleteTask(element) {
+  console.log("DELETE TASK");
+}
+
+function tagTask(element) {
+  console.log("TAG TASK");
 }
